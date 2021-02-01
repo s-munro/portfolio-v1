9f3d1e6e79478c93b1624c1ee5ba23feb15239cb
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-01 04:34:21 -0500

    create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./g-components/Nav";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Nav />
+      <main className="row">
+        <div className="col-12">
+          <h1>Hello, world!</h1>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/g-components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="row w-100">
+      <div>Hi!</div>
+    </nav>
+  );
+};
+
+export default Nav;
