a128690403e6da15509f235abbdb28e83becae8b
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-05 13:59:54 -0500

    easyscore section

--- src/screens/projects/ProjectGallery.js
+++ src/screens/projects/ProjectGallery.js
@@ -1,10 +1,12 @@
 import React from "react";
 import EasyScoreSection from "./EasyScoreSection";
+import VRFunderSection from "./VRFunderSection";
 
 const ProjectGallery = () => {
   return (
     <div className="col-12">
       <EasyScoreSection />
+      <VRFunderSection />
     </div>
   );
 };
---
+++ src/screens/projects/VRFunderSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+import gif from "../../assets/easyscore-giphy-demo.gif";
+
+const VRFunderSection = () => {
+  return (
+    <div className="row w-100 bottom-margin-twenty">
+      <div
+        id="easy-score"
+        className="col-12 d-flex flex-column align-items-center justify-content-center mb-5"
+      >
+        <h2 className="project-heading">VR-Funder</h2>
+        <h6>An advanced course-planning resource for college students</h6>
+      </div>
+      <div className="col-4 d-flex flex-column align-items-start justify-content-center">
+        <p className="stat-title">Lead Developer</p>
+        <p className="stat">
+          Organized and implemented site design and development
+        </p>
+        <p className="stat">React, Redux, Node.js</p>
+        <p className="stat">Material-UI, Chart.js, Fuse.js, Nodemailer</p>
+        <p className="stat-center">
+          <a
+            href="https://github.com/s-munro/easyscore"
+            className="stat-link animated-underline-3 underline-base"
+          >
+            Github
+          </a>
+          <span className="secondary"> | </span>
+          <a
+            href="https://the-easy-score.herokuapp.com/"
+            className="stat-link animated-underline-3 underline-base"
+          >
+            Demo
+          </a>
+        </p>
+        <br />
+        <h5>
+          <span className="secondary animated-underline-2 underline-base pseudo-link">
+            View
+          </span>{" "}
+          Full Case Study
+        </h5>
+      </div>
+      <div className="col-8 d-flex align-items-center justify-content-center">
+        <div className="gif">
+          <img width="600px" src={gif} alt="easyscore gif" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VRFunderSection;
