f2b7bf2fa1d291d1cd8a4b279ea370e9d5efab59
Sam <sjm.munro@gmail.com>
2021-04-04 12:03:32 -0400

    chore: moved take tour text to a component, display none conditional added for view workaround

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,6 +3,7 @@ import "../../App.css";
 import { BsArrowReturnRight } from "react-icons/bs";
 import { useHistory } from "react-router-dom";
 import AnimatedHeader from "./AnimatedHeader";
+import SecondaryText from './SecondaryText';
 import "./home.css";
 
 const Home = (props) => {
@@ -15,19 +16,12 @@ const Home = (props) => {
   return (
     <div className="container">
       <div className="row w-100 main-body d-flex justify-content-center align-items-center">
-        <div className="col-lg-6 col-md-12 d-flex flex-column justify-content-center align-items-center">
+        <div className="col-xl-6 col-lg-6 col-md-12 d-flex flex-column justify-content-center align-items-center">
           <AnimatedHeader />
           <p data-aos="fade-up" data-aos-delay="400">Software Engineer</p>
+          <SecondaryText desktop={false} handleTourClick={handleTourClick} />
         </div>
-        <div data-aos="fade-up" data-aos-delay="1300" className="col-6 d-flex flex-column justify-content-center align-items-center">
-          <h5>
-            Take&nbsp;the&nbsp; 
-            <span className="secondary link" onClick={handleTourClick}>
-                tour
-            </span>
-          </h5>
-          <BsArrowReturnRight />
-        </div>
+        <SecondaryText desktop={true} handleTourClick={handleTourClick}/>
       </div>
     </div>
   );
